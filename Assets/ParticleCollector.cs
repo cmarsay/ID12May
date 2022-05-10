c08e868f47e202166df709dacedc5a171adcc854
cmarsay <98067756+cmarsay@users.noreply.github.com>
2022-05-10 17:46:01 +0100

    10May17:35

---
+++ Assets/ParticleCollector.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleCollector : MonoBehaviour
+{ 
+
+    ParticleSystem ps;
+
+    List<ParticleSystem.Particle> particles = new List<ParticleSystem.Particle>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    private void OnParticleTrigger()
+    {
+        int triggeredParticles = ps.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
+
+        for (int i=0; i < triggeredParticles; i++)
+        {
+            ParticleSystem.Particle p = particles[i];
+            p.remainingLifetime = 0;
+            Debug.Log("Collected");
+            particles[i] = p;
+        }
+
+        ps.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, particles);
+
+    }
+}
